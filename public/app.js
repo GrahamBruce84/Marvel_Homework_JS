6b0ea1818dffa1dc02027f38517cf4095b626522
Graham Bruce <grahambruce84@gmail.com>
2017-08-05 18:00:04 +0100

    Trying to get the URL to work

--- public/app.js
+++ public/app.js
@@ -1,6 +1,5 @@
 var app = function(){
   randomComic();
-  makeRequest(url, requestComplete);
 }
 
 var makeRequest = function(url, callback){
@@ -17,13 +16,21 @@ var requestComplete = function(){
 };
 
 var randomComic = function(){
+  var button = document.getElementById('#start-button');
   var PRIV_KEY = "0823f7d5b0592cc5ffc18f7e052fe4b93fe5a71c";
   var API_KEY = "2331b1ccc2a9411ca23da8c89a441eb2";
   var ts = new Date().getTime();
   var randomNumber = getRandomInt(1, 50000);
       console.log(randomNumber);
   var url = "http://gateway.marvel.com:80/v1/public/comics/" + randomNumber + "?apikey=2331b1ccc2a9411ca23da8c89a441eb2";
-  var hash = md5(ts + PRIV_KEY + API_KEY);
+  var hash = ts + PRIV_KEY + API_KEY;
   url += "&ts="+ts+"&hash="+hash;
+  makeRequest(url, requestComplete);
 };
 
+var getRandomInt = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+
+window.addEventListener('load', app);
